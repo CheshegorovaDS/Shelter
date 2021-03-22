95241c233c54aa69c11029de38b79baca2d0a118
dcheshegorova <peschink@mail.ru>
2021-03-22 19:04:31 +0600

    delete mvp news package

--- Shelter/app/src/main/java/com/example/shelter/presentation/fragment_menu/news/model/INewsModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shelter.presentation.fragment_menu.news.model
-
-import com.example.shelter.presentation.model.Animal
-import com.example.shelter.presentation.model.News
-
-interface INewsModel {
-    fun loadNews(): List<News>
-}
